af1c1843f2879982b29c5ba28c00c4e6aec03726
Edson Sales <edsonsales99@gmail.com>
2021-12-07 09:21:28 -0500

    Criação do projeto

---
+++ src/test/java/com/delivery/webservicejpa/WebservicejpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.delivery.webservicejpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebservicejpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
