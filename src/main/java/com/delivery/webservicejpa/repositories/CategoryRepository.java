15e14c8226b5e426b219649425879d9df3d97374
Edson Sales <edsonsales99@gmail.com>
2022-01-03 23:05:25 -0500

    Category

---
+++ src/main/java/com/delivery/webservicejpa/repositories/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.delivery.webservicejpa.repositories;
+
+import com.delivery.webservicejpa.entities.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface CategoryRepository extends JpaRepository<Category, Long>{
+
+}
---
+++ src/main/java/com/delivery/webservicejpa/services/CategoryService.java
@@ -0,0 +1,28 @@
+package com.delivery.webservicejpa.services;
+
+import com.delivery.webservicejpa.entities.Category;
+import com.delivery.webservicejpa.entities.User;
+import com.delivery.webservicejpa.repositories.CategoryRepository;
+import com.delivery.webservicejpa.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class CategoryService {
+	
+	@Autowired
+	private CategoryRepository repository;
+	
+	public List<Category> findAll(){
+		return repository.findAll();
+	}
+	
+	public Category findById(Long id) {
+		Optional<Category> obj =  repository.findById(id);
+		return obj.get();
+	}
+	
+}
