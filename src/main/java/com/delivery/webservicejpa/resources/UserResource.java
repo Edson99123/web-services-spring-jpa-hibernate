edf3f91880c9270cf19f902f5db797e7bb4ad5cf
Edson Sales <edsonsales99@gmail.com>
2022-11-11 10:45:10 -0400

    User insert

--- src/main/java/com/delivery/webservicejpa/resources/UserResource.java
+++ src/main/java/com/delivery/webservicejpa/resources/UserResource.java
@@ -4,19 +4,19 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import java.net.URI;
+
 
 import com.delivery.webservicejpa.entities.User;
 import com.delivery.webservicejpa.services.UserService;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 	
-	@Autowired	
+	@Autowired
 	private UserService service;
 	
 	@GetMapping
@@ -31,4 +31,11 @@ public class UserResource {
 		return ResponseEntity.ok().body(obj);
 	}
 
+	@PostMapping
+	public ResponseEntity<User> insert(@RequestBody User obj){
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).body(obj);
+	}
 }
